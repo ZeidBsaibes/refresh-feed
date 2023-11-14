152182ada275dcf5537951d766a1acfe30f34a19
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-14 17:12:03 +0000

    prisma config for Google Login

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,5 +1,10 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import GithubProvider from "next-auth/providers/github";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import { PrismaClient } from "@prisma/client";
+import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
+
+const prisma = new PrismaClient();
 
 export const authOptions = {
   // Configure one or more authentication providers
@@ -8,9 +13,15 @@ export const authOptions = {
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
     }),
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
     // ...add more providers here
   ],
-}
+  adapter: PrismaAdapter(prisma),
+};
 
-export default NextAuth(authOptions)
+const handler = NextAuth(authOptions);
 
+export { handler as GET, handler as POST };
