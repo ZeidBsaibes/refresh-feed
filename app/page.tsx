c5b06cd58fa3094c138bec74cb8bee809db48f90
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-15 21:10:05 +0000

    added homepage placeholder

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,14 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import LandingHero from "./components/LandingHero/LandingHero";
 
 export default async function Home() {
   const session = await getServerSession(authOptions);
 
   console.log(session);
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Home Page</h1>
+    <main className="flex min-h-screen flex-col items-center justify-between p-0">
+      <LandingHero />
     </main>
   );
 }
