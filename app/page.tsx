7c4e21eb5e7b411e436f01bc2c45557918b2237e
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-21 17:08:18 +0000

    more auth fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 import { getServerSession } from "next-auth";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "../lib/auth";
 import LandingHero from "./components/LandingHero/LandingHero";
 import Head from "next/head";
 
