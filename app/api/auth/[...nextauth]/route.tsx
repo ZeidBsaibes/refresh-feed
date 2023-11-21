27f148d0b87eba72079f5fad7fa17ce547bfcc2d
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-21 16:07:53 +0000

    refactored next auth options and moved to lib folder

--- app/api/auth/[...nextauth]/route.tsx
+++ app/api/auth/[...nextauth]/route.tsx
@@ -1,37 +1,5 @@
-// @ts-nocheck
-
-import GoogleProvider from "next-auth/providers/google";
+import { authOptions } from "@/lib/auth";
 import NextAuth from "next-auth";
-import type { AuthOptions } from "next-auth";
-import { PrismaClient } from "@prisma/client";
-import { PrismaAdapter } from "@auth/prisma-adapter";
-
-const prisma = new PrismaClient();
-
-export const authOptions: AuthOptions = {
-  session: { strategy: "jwt" },
-  adapter: PrismaAdapter(prisma),
-  providers: [
-    GoogleProvider({
-      clientId: process.env.GOOGLE_ID,
-      clientSecret: process.env.GOOGLE_SECRET,
-    }),
-  ],
-  callbacks: {
-    async jwt({ token, account, user }) {
-      // Persist the OAuth access_token to the token right after signin
-      if (user) {
-        token.userId = user.id;
-      }
-      return token;
-    },
-    async session({ session, token }) {
-      // Send properties to the client, like an access_token from a provider.
-      session.user.userId = token.userId;
-      return session;
-    },
-  },
-};
 
 const handler = NextAuth(authOptions);
 
