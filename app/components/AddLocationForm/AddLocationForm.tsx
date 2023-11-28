ac008119ab351763fc4f508a374efb9070b7197e
Zeid Bsaibes <zeid.bsaibes@gmail.com>
2023-11-28 13:57:45 +0000

    allow form scroll on mobile

--- app/components/AddLocationForm/AddLocationForm.tsx
+++ app/components/AddLocationForm/AddLocationForm.tsx
@@ -140,7 +140,10 @@ export default function AddLocationForm() {
   };
 
   return (
-    <form onSubmit={handleFormSubmit} className="bg-white px-8 py-8 ">
+    <form
+      onSubmit={handleFormSubmit}
+      className="bg-white px-8 py-20 h-full overflow-auto"
+    >
       <div className="space-y-12">
         <div className="border-b border-gray-900/10 pb-12">
           <h2 className="text-base font-semibold leading-7 text-gray-900">
